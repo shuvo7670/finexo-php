30000623d084e626fa8c943703add20f2728e6b7
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-05-09 11:45:18 +0600

    added slider content

--- index.php
+++ index.php
@@ -12,91 +12,57 @@
     </div>
    <?php include_once './partials/header.php' ?>
     <!-- end header section -->
+     <?php
+        $sliders = [
+            [
+                'slider_title'   => 'Crypto <br> Currency 1',
+                'slider_content' => 'Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.',
+                'button_text'    => 'Read More',
+                'image_url'      => 'images/slider-img.png',
+            ],
+            [
+                'slider_title'   => 'Crypto <br> Currency 2',
+                'slider_content' => 'Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.',
+                'button_text'    => 'Read More',
+                'image_url'      => 'images/slider-img.png',
+            ],
+            [
+                'slider_title'   => 'Crypto <br> Currency 3',
+                'slider_content' => 'Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.',
+                'button_text'    => 'Read More',
+                'image_url'      => 'images/slider-img.png',
+            ],
+        ]
+     
+     ?>
     <!-- slider section -->
     <section class="slider_section ">
       <div id="customCarousel1" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="container ">
-              <div class="row">
-                <div class="col-md-6 ">
-                  <div class="detail-box">
-                    <h1>
-                      Crypto <br>
-                      Currency
-                    </h1>
-                    <p>
-                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
-                    </p>
-                    <div class="btn-box">
-                      <a href="" class="btn1">
-                        Read More
-                      </a>
+            <?php foreach( $sliders as $index => $slide ) : ?>
+                <div class="carousel-item <?php echo $index == 0 ? 'active' : ''  ?> ">
+                    <div class="container ">
+                    <div class="row">
+                        <div class="col-md-6 ">
+                        <div class="detail-box">
+                            <h1><?php echo $slide['slider_title'] ?></h1>
+                            <p><?php echo $slide['slider_content'] ?></p>
+                            <div class="btn-box">
+                            <a href="" class="btn1">
+                                <?php echo $slide['button_text'] ?>
+                            </a>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col-md-6">
+                        <div class="img-box">
+                            <img src="<?php echo $slide['image_url'] ?>" alt="">
+                        </div>
+                        </div>
                     </div>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="img-box">
-                    <img src="images/slider-img.png" alt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item ">
-            <div class="container ">
-              <div class="row">
-                <div class="col-md-6 ">
-                  <div class="detail-box">
-                    <h1>
-                      Crypto <br>
-                      Currency
-                    </h1>
-                    <p>
-                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
-                    </p>
-                    <div class="btn-box">
-                      <a href="" class="btn1">
-                        Read More
-                      </a>
                     </div>
-                  </div>
                 </div>
-                <div class="col-md-6">
-                  <div class="img-box">
-                    <img src="images/slider-img.png" alt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="container ">
-              <div class="row">
-                <div class="col-md-6 ">
-                  <div class="detail-box">
-                    <h1>
-                      Crypto <br>
-                      Currency
-                    </h1>
-                    <p>
-                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
-                    </p>
-                    <div class="btn-box">
-                      <a href="" class="btn1">
-                        Read More
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="img-box">
-                    <img src="images/slider-img.png" alt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            <?php endforeach ?>
         </div>
         <ol class="carousel-indicators">
           <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
